3a5d6f1d8afd1c9a35fb9566af7d90a34926c351
Alexander Richard Vega Fuentes <alexander5243188@yahoo.es>
2024-04-01 20:35:13 -0400

    creamos la variable proceso y asignamos un valor

--- estado.c
+++ estado.c
@@ -2,3 +2,10 @@
 
 //Definimos el  tipo de dato
 enum ESTADO {EXIT, OK };
+
+int main(){
+    //Variable 
+    enum ESTADO proceso;
+    proceso = OK; //Asignamos el valor OK a la variale
+    retun 0;
+}
