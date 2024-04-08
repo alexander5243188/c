983fd5ae0c67ce5a6ad4a70c3f29809ec825dfba
Alexander Richard Vega Fuentes <alexander5243188@yahoo.es>
2024-04-07 20:39:04 -0400

    declaracion de variable alto e ingreso del mismo

--- parametrosRectangulo.c
+++ parametrosRectangulo.c
@@ -1,5 +1,10 @@
 /*Ingrese como parametros el ancho y alto de un rectángulo*/
 #include <stdio.h>
 int main(){
-    int alto, ancho;    
+    int alto;
+
+    printf("Alto: ");
+    scanf("%d", &alto);
+
+    return 0;
 }
