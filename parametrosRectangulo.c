07cbb51e1be429abbd6629f4490d8bc6d6d58c83
Alexander Richard Vega Fuentes <alexander5243188@yahoo.es>
2024-04-07 20:52:39 -0400

    ciclo for para el salto de linea y llenado del rectangulo

--- parametrosRectangulo.c
+++ parametrosRectangulo.c
@@ -9,5 +9,25 @@ int main(){
     printf("Ancho: ");
     scanf("%d", &ancho);
 
+    if (alto < ancho || alto > ancho)
+    {
+        /* code */
+        for (size_t i = 0; i < alto; i++)
+        {
+            /* code */
+            for (size_t j = 0; j < ancho; j++)
+            {
+                /* code */
+                printf("*");
+            }
+            printf("\n");
+            
+        }
+        
+    }else{
+        printf("Alto: %d, Ancho: %d, no son las dimenciones de un rectangulo \n", alto, ancho);
+    }
+    
+
     return 0;
 }
